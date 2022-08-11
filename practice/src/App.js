d5b704bcc4f57a18939c7ac5402b7214a58a4088
Trent Pfeiffer <trentpf@hotmail.com>
2022-08-11 15:26:54 -0400

    Add initial components

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header() {
+  return(
+    <header>
+      <nav>
+      <img src={logo} alt="logo" width="40px"/>
+      </nav>
+    </header>  
+  )
+};
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+        <h1>Website Title/Description</h1>
+        <ol>
+          <li>lorem ipsum</li>
+          <li>lorem ipsum</li>
+          <li>lorem ipsum</li>
+        </ol>
     </div>
   );
 }
