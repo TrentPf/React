47a7bb376735db85f5f04b31534aff579897a1fc
Trent Pfeiffer <trentpf@hotmail.com>
2022-10-03 15:28:11 -0400

    Add initial navbar layout

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,28 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+import './style.css';
 
-function Header() {
-  return(
-    <header>
-      <nav>
-      <img src={logo} alt="logo" width="40px"/>
-      </nav>
-    </header>  
-  )
-};
-
-function App() {
+export default function App() {
   return (
-    <div>
-      <Header />
-        <h1>Website Title/Description</h1>
-        <ol>
-          <li>lorem ipsum</li>
-          <li>lorem ipsum</li>
-          <li>lorem ipsum</li>
-        </ol>
+    <div className='container'>
+      <Navbar />
+      <Main />
     </div>
   );
-}
-
-export default App;
+};
---
+++ practice/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src="../images/icons8-cocktail-64.png" alt="cocktail glass"/>
+      <h1>Navbar Placeholder</h1>
+    </nav>
+  );
+};
